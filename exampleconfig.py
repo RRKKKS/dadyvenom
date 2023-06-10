22a8c2d171c5d9260bafed199e118168348423e6
dadyvenom <135177210+dadyvenom@users.noreply.github.com>
2023-06-10 11:42:55 +0300

    Add files via upload

---
+++ exampleconfig.py
@@ -0,0 +1,24 @@
+from sample_config import Config
+
+#لمنصبي السيرفر
+
+class Development(Config):
+    # احصل على الايبيات من الموقع the my.telegram.org
+    APP_ID = 26022994
+    API_HASH = "2e84a6b68bd6b5a79f46e8192668e0eae"
+    # اسم حسابك
+    ALIVE_NAME = "𝐘𝐄𝐒 ,𝐈𝐌, 𝐕𝐄𝐍𝐎𝐌"
+    # اصنع رابط تخزين منelephantsql
+    DB_URI = "postgres://tqhzabkm:yvV54LrL3V7VVY-fpsTxWFfoXwA8jqY2@rajje.db.elephantsql.com/tqhzabkm"
+    # كود سيشن تيليثون (كود تيرمكس )
+    STRING_SESSION = "BAB4y0rKdjhcbzb_cYDoT89BAXO8g9-wUCzc7jXnqfcCdaj7Slp3pHtBO9MvARn06Fq0abBSoWUzy-fR1L6vSaYSOnnQ58O5gesy6hssksLk_asgPui7uHVzMyeAuj0zEGdM-sQeiYLNjud3dV5Lf3IkoAb4U0qKqFg5VIWdcnLTuBR6jxDSPtqVuqc55_E4yV9QUzAuyBWfG8ZkFbzK1EPnTQYh38s_Y8L4fDyE57djmUdz5z_NkVjerfG3JipBjpfom_xerOE1C_DCz4WogHM1xGDNOVLRhKu16hl7uvP6FHIdTH3d52qMvQwnRfY_uYAaRK7-euUcLBzucGHjxS0QAAAAAT6f8toA"
+    # اوكن بوتك
+    TG_BOT_TOKEN = "5855909128:AAFAugGDOZmRFdHxd806D366TnxQZd_WbVU"
+    # هنا اصنع كروب خاص وضع ايديه هنا
+    PRIVATE_GROUP_BOT_API_ID = -868013343
+    # لا تغيرها
+    COMMAND_HAND_LER = "."
+    # لا تغيرها
+    SUDO_COMMAND_HAND_LER = "."
+    # رابط ملفات الاوامر الاضافيه لا تغيره
+    EXTERNAL_REPO = "https://github.com/sbb_b/JmPlugins"
